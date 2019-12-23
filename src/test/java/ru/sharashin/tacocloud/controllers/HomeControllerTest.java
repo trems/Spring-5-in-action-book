85e8e7ccd914e04ae55f637530d2da979dccd103
sharashin.mikhail <sharashin.mikhail@otr.ru>
2019-12-23 14:25:57 +0500

    Added homepage controller, view, test

---
+++ src/test/java/ru/sharashin/tacocloud/controllers/HomeControllerTest.java
@@ -0,0 +1,26 @@
+package ru.sharashin.tacocloud.controllers;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(HomeController.class)
+public class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHomePage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect(content().string(Matchers.containsString("Welcome to...")));
+    }
+
+}
